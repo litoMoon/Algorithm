94b8f97135cbc2f170a9404a2a2ee0cc2bec5cd2
yqsngdg <litomoon@163.com>
2026-01-20 22:35:33 +0800

    algorithm

---
+++ src/LeetCode/RandomListCopy.java
@@ -0,0 +1,34 @@
+package LeetCode;
+
+import java.util.HashMap;
+
+public class RandomListCopy {
+    public Node copyRandomList(Node head) {
+        HashMap<Node,Node> map=new HashMap<>();
+        Node cur=head;
+        while(cur!=null){
+            Node node=new Node(cur.val);
+            map.put(cur,node);
+            cur=cur.next;
+        }
+        Node resultNode=map.get(head);
+        cur=head;
+        while(cur!=null){
+            map.get(cur).next=map.get(cur.next);
+            map.get(cur).random=map.get(cur.random);
+            cur=cur.next;
+        }
+        return resultNode;
+    }
+}
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
