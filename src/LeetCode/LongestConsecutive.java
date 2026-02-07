85c705147b7bb8129e4a2439159714e871d69305
litomoon <litomoon@163.com>
2026-02-07 22:46:13 +0800

    algorithm

---
+++ src/LeetCode/LongestConsecutive.java
@@ -0,0 +1,33 @@
+package LeetCode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestConsecutive {
+
+    public int longestConsecutive(int[] nums) {
+
+        Set<Integer> st = new HashSet<>();
+        for (int num : nums) {
+            st.add(num); // 把 nums 转成哈希集合
+        }
+
+        int ans = 0;
+        for (int x : st) { // 遍历哈希集合
+            if (st.contains(x - 1)) { // 如果 x 不是序列的起点，直接跳过
+                continue;
+            }
+            // x 是序列的起点
+            int y = x + 1;
+            while (st.contains(y)) { // 不断查找下一个数是否在哈希集合中
+                y++;
+            }
+            // 循环结束后，y-1 是最后一个在哈希集合中的数
+            ans = Math.max(ans, y - x); // 从 x 到 y-1 一共 y-x 个数
+        }
+
+        return ans;
+
+    }
+
+}
