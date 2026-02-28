a2f8429b9e6dae9c6dea71e1812fe35c44ff9977
litomoon <litomoon@163.com>
2026-02-28 21:11:13 +0800

    algorithm

---
+++ src/LeetCode/InvertTree.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+public class InvertTree {
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null){
+            return root;
+        }
+        TreeNode tmp;
+        tmp=root.left;
+        root.left=root.right;
+        root.right=tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+
+    }
+}
