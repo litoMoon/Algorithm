090fa7362685e16a28446b1b41405dfac516c177
litomoon <litomoon@163.com>
2026-03-06 23:35:12 +0800

    algorithm

---
+++ src/LeetCode/LongestPalindrome.java
@@ -0,0 +1,38 @@
+package LeetCode;
+
+public class LongestPalindrome {
+    public String longestPalindrome(String s) {
+        if(s.length()==0) return null;
+        //   if(s.length()==1) return s;
+        int mid=0,maxlen=0,ret=0;
+        while(mid<s.length()){
+            // left 和 right 同时往两边走
+            int left=mid,right=mid;
+            while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)){
+                if((right-left+1)>maxlen){
+                    ret=left;
+                    maxlen=right-left+1;
+                }
+                left--;
+                right++;
+
+            }
+
+
+            //计算偶数的可能
+            left=mid; right=mid+1;
+            while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)){
+                if((right-left+1)>maxlen){
+                    ret=left;
+                    maxlen=right-left+1;
+                }
+                left--;
+                right++;
+
+            }
+
+            mid++;
+        }
+        return s.substring(ret,ret+maxlen);
+    }
+}
