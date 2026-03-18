26d807a662f6a19314c3c73048480f88a4938aa6
litomoon <litomoon@163.com>
2026-03-18 18:03:28 +0800

    algorithm

--- src/LeetCode/SearchMatrix.java
+++ src/LeetCode/SearchMatrix.java
@@ -2,39 +2,22 @@ package LeetCode;
 
 public class SearchMatrix {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int rowIndex = binarySearchFirstColumn(matrix, target);
-        if (rowIndex < 0) {
-            return false;
-        }
-        return binarySearchRow(matrix[rowIndex], target);
-    }
-
-    public int binarySearchFirstColumn(int[][] matrix, int target) {
-        int low = -1, high = matrix.length - 1;
-        while (low < high) {
-            int mid = (high - low + 1) / 2 + low;
-            if (matrix[mid][0] <= target) {
-                low = mid;
-            } else {
-                high = mid - 1;
-            }
-        }
-        return low;
-    }
-
-    public boolean binarySearchRow(int[] row, int target) {
-        int low = 0, high = row.length - 1;
-        while (low <= high) {
-            int mid = (high - low) / 2 + low;
-            if (row[mid] == target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = -1;
+        int right = m * n;
+        while (left + 1 < right) {
+            int mid = (left + right) >>> 1;
+            int x = matrix[mid / n][mid % n];
+            if (x == target) {
                 return true;
-            } else if (row[mid] > target) {
-                high = mid - 1;
+            }
+            if (x < target) {
+                left = mid;
             } else {
-                low = mid + 1;
+                right = mid;
             }
         }
         return false;
     }
-
 }
