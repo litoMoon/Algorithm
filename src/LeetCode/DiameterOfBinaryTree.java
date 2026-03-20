0171c0c1e9e159dac7fd9a0815a75edf95f19927
litomoon <litomoon@163.com>
2026-03-20 19:17:58 +0800

    algorithm

---
+++ src/LeetCode/DiameterOfBinaryTree.java
@@ -0,0 +1,20 @@
+package LeetCode;
+
+public class DiameterOfBinaryTree {
+    private int ans;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+
+    private int dfs(TreeNode node) {
+        if (node == null) {
+            return -1; // 对于叶子来说，链长就是 -1+1=0
+        }
+        int lLen = dfs(node.left) + 1; // 左子树最大链长+1
+        int rLen = dfs(node.right) + 1; // 右子树最大链长+1
+        ans = Math.max(ans, lLen + rLen); // 两条链拼成路径
+        return Math.max(lLen, rLen); // 当前子树最大链长
+    }
+}
