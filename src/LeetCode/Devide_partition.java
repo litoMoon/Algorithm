2c069fd4a823a53f5a122f9a83fbc365d50bcea2
litomoon <litomoon@163.com>
2026-03-23 23:48:39 +0800

    algorithm

---
+++ src/LeetCode/Devide_partition.java
@@ -0,0 +1,38 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Devide_partition {
+    public List<List<String>> partition(String s) {
+        List<List<String>> res=new ArrayList<>();
+        List<String> path=new ArrayList<>();
+        isPalindrome(0,s,path,res);//0表示遍历的起点从字符串第一个字符开始
+        return res;
+    }
+    public void isPalindrome(int i,String s,List<String> path,List<List<String>> res){
+        if(i==s.length()){
+            res.add(new ArrayList<>(path));
+            return ;
+        }
+        //遍历当前字符串
+        for(int j=i;j<s.length();j++){
+            if(yesPalindrome(i,j,s)){
+                //true表示为回文串
+                path.add(s.substring(i,j+1));
+                isPalindrome(j+1,s,path,res);
+                path.remove(path.size()-1);
+            }
+        }
+
+
+    }
+    public boolean yesPalindrome(int left,int right,String s){
+        while(left<right){
+            if(s.charAt(left++)!=s.charAt(right--)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
